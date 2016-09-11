de8c252086ea777a024d91e0de87ad37044e1370
Nick Maher <nick.maher@appearhere.co.uk>
2016-09-11 01:01:36 +0100

    Once more with Logging

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem "http"
 gem "pry"
+gem "dotenv"
 
 group :rpi do
   gem 'wiringpi2'
--- lib/pi.rb
+++ lib/pi.rb
@@ -1,9 +1,31 @@
-require 'pi/segment_display'
-require 'pi/tfl'
-
-require 'pi/monitors/next_bus'
+require 'logger'
+require 'multi_io'
+require 'dotenv'
+Dotenv.load
 
 module Pi
+  module_function
 
+  def gpio_available?
+    !(ENV.fetch('PREVENT_GPIO', false) == 'true')
+  end
 
+  def loop_with_error_handling(sleep_for:, &block)
+    loop do
+      begin
+        yield
+      rescue StandardError => e
+        logger.error(e)
+      ensure
+        sleep(sleep_for)
+      end
+    end
+  end
+
+  def logger
+    @logger ||= begin
+      log_file = File.open("log/pi.log", "a")
+      Logger.new(MultiIO.new(STDOUT, log_file))
+    end
+  end
 end
